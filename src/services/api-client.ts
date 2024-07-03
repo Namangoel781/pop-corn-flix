4bfe653e81565070004ce21a30921c3968e8d47a
Naman Goel <namangoel781@gmail.com>
2024-07-03 14:55:43 +0530

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: 'b11027c07eef0fa27665e605faaf1c8a'
+        api_key: 'YOUR_API_KEY'
     },
 });
